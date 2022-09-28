87ca4d3632ae406ffe21f162838316fe4341d847
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-28 11:16:19 -0400

    implement Auth Middleware to populate Me Query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,7 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
+  
   type User {
     _id: ID
     username: String
@@ -10,25 +9,38 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
-
   type Book {
-    bookId: ID
-    authors: [author]
+    bookId: String
+    authors: [String]
     description: String
     title: String
     image: String
     link: String
   }
-
   type Auth {
-    token: ID
+    token: ID!
     user: User
   }
-
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
   type Query {
     me: User
+    users: [User]
+    user(username: String!): User
   }
-`;
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
+  }
+  `;
 
-// export the typeDefs
-module.exports = typeDefs;
+// export the typeDef
+module.exports = typeDefs; 
